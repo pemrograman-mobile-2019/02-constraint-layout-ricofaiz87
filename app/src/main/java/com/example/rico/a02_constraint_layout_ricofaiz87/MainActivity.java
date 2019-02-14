d8fa0b4531a307bd8a7d740994cd9e5f91808310
ricofaiz87 <rico.faiz19@gmail.com>
2019-02-14 22:04:42 +0700

    constrain layout

---
+++ app/src/main/java/com/example/rico/a02_constraint_layout_ricofaiz87/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rico.a02_constraint_layout_ricofaiz87;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
